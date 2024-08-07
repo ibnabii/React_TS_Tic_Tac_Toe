678269bc04becebbb71c91699fc0a0047cdcac19
ibnabii <someone@somewhere.net>
2024-08-07 23:26:03 +0200

    Editing player name

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,33 @@
-export type PlayerProps = {
-  name: string;
+import { useState } from "react";
+
+type PlayerProps = {
   symbol: string;
 };
-export default function Player({ name, symbol }: PlayerProps) {
+
+export default function Player({ symbol }: PlayerProps) {
+  const [name, setName] = useState<string>(" ");
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  const editName = (
+    <input
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      required
+      type="text"
+    />
+  );
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        <span className="player-name">{isEditing ? editName : name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
